47f5e78697526bbf5a916b123d5935bd369b85d2
Plat <github@p1at.dev>
2022-09-24 21:43:20 +0900

    feat: update icon color variation

--- pages/api/icon.ts
+++ pages/api/icon.ts
@@ -1,4 +1,4 @@
-import { toPng } from "jdenticon"
+import { toPng, JdenticonConfig } from "jdenticon"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -11,7 +11,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     res.setHeader("Content-Type", "image/png")
 
-    res.status(200).send(toPng(username, 128))
+    // Custom identicon style
+    // https://jdenticon.com/icon-designer.html?config=2a194bff01413a132d57295f
+    const config: JdenticonConfig = {
+        lightness: {
+            color: [0.46, 0.88],
+            grayscale: [0.42, 0.95],
+        },
+        saturation: {
+            color: 0.59,
+            grayscale: 0.2,
+        },
+        backColor: "#191e4bff",
+    }
+
+    res.status(200).send(toPng(username, 128, config))
 }
 
 export default handler
